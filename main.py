cbd1b718656f19e9057c33496dd7ba59a65e8b3b
GustavoRocha <@rocha.gustavops@gmail.com>
2024-03-26 11:39:46 -0300

    Controller

--- main.py
+++ main.py
@@ -9,12 +9,31 @@ class Task:
 
 #View
 class Taskview:
-  def show_users(self, users):
-    print("Usuários")
-    for user in users:
-      print(f"Nome: {user.name}, Email: {user.email}")
-
   def show_tasks(self, tasks):
-    print("\nTarefas:")
+    print("Tarefas:")
     for task in tasks:
-      print(f"Título: {task.titulo}, Descrição: {task.description}, Aberto em: {task.dateOpen}, Fechado em: {task.dateClose}")
+      print(f"Título: {task.titulo}, Descrição: {task.descricao}, Aberto em: {task.dateOpen}, Fechado em: {task.dateClose}")
+
+
+#Controller
+class TaskController:
+    def __init__(self, model, view):
+        self.model = model
+        self.view = view
+
+    def add_task(self, titulo, descricao, dateOpen, dateClose):
+        task = Task(titulo, descricao, dateOpen, dateClose)
+        self.model.add_task(task)
+        self.update_view()
+
+    def mark_task_as_completed(self, task_index):
+        self.model.mark_task_as_completed(task_index)
+        self.update_view()
+
+    def get_task_list(self):
+        return self.model.get_task_list()
+
+    def update_view(self):
+        tasks = self.get_task_list()
+        self.view.show_tasks(tasks)
+      
